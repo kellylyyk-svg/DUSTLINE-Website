b6612a59cbc23e0ca0bf87fcc339aeb0557c69f1
kellylyyk-svg <kellylyyk@gmail.com>
2025-12-24 17:48:03 +0900

    sub2 완성

--- script/action.js
+++ script/action.js
@@ -1,13 +1,48 @@
 
-let number = 1
-$('.btn_next').click(function(){
-    $('.hero ul').css({marginLeft:-number*100+'%'})
-    console.log(number)
+// let number = 1
+// $('.btn_next').click(function(){
+//     $('.hero ul').css({marginLeft:-number*100+'%'})
+//     console.log(number)
+// })
+
+// $('.btn_prev').click(function(){
+//     $('.hero ul').css({marginLeft:'0'})
+// })
+
+
+$('.btn_af').click(function(){
+    if($('.section1 ul').is(':animated')) return;
+
+    let k = $('.section1 ul').width();
+    $('.section1 ul').animate({marginLeft: -k-30}, 1000, function(){
+        $('.section1 ul li').slice(0, 3).appendTo('.section1 ul');
+        $('.section1 ul').css({marginLeft: 0})
+    })
 })
 
-$('.btn_prev').click(function(){
-    $('.hero ul').css({marginLeft:'0'})
+$('.btn_be').click(function(){
+    if($('.section1 ul').is(':animated')) return;
+    $('.section1 ul li:last-child').prependTo('.section1 ul');
+    $('.section1 ul').css({marginLeft: '-100%'});
+    $('.section1 ul').animate({marginLeft: 0}, 500);
+});
+
+
+let currentDegree = 0;
+
+$('.cta').click(function(){
+    currentDegree += 360;
+    $(this).css({
+        'transform': 'rotate(' + currentDegree + 'deg)',
+        'transition': '1s'
+    })
 })
+
+$('.gnb > li').click(function(){
+    $('.gnb > li').removeClass('on')
+    $(this).addClass('on')
+})
+
 
 
 
